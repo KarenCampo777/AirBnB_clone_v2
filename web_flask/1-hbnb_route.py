d9e46264078c98fb93790a8d36cfadbef34f1c3e
KarenCampo777 <1472@holbertonschool.com>
2021-01-21 04:12:57 +0000

    adding task with style corrections

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,27 +2,26 @@
 """
 script that starts a Flask web application:
 """
-from flask import Flask  
+from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
-"""
-Prints Hello HBNB!"
-"""
-    return ("Hello HBNB!")
+
+    """ Prints Hello HBNB!" """
+return ("Hello HBNB!")
 
 
 @app.route('/', strict_slashes=False)
-def hbnb(): 
-"""
-Prints "HBNB"
-"""      
-    return  "HBNB"
+def hbnb():
+    """ Prints "HBNB" """
+
+
+return "HBNB"
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
 """Runs sever port: 5000"
 """
-
