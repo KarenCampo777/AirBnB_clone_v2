e1572c7f5cf264bb40d442f3b2e939369d5a41c4
KarenCampo777 <1472@holbertonschool.com>
2021-02-26 00:07:46 +0000

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Fabric Script
+"""
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """ Generate a .tgz archive from     web_static folder """
+    time = datetime.now()
+    name = 'web_static_' + str(time.year) + str(time.month) + str(time.day)
+    name = name + str(time.hour) + str(time.minute) + str(time.second) + '.tgz'
+    local('mkdir -p versions')
+    archive = local('tar -cvzf versions/{} web_static'.format(name))
+    if archive.failed:
+        return None
+    return 'versions/{}'.format(name)
